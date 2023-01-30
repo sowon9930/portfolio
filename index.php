140fd12dc9ef3868d356485152723fecd8048a79
김소진 <thwls9930@gmail.com>
2023-01-30 14:48:44 +0900

    구조 설계

---
+++ index.php
@@ -0,0 +1,13 @@
+  <?php include "header.php" ?>
+  <main class="main">
+    <?php include 'home.php' ?>
+    <?php include 'about.php' ?>
+    <?php include 'work1.php' ?>
+    <?php include 'work2.php' ?>
+    <?php include 'work3.php' ?>
+    <?php include 'work4.php' ?>
+    <?php include 'work5.php' ?>
+    <?php include 'work6.php' ?>
+    <?php include 'contact.php' ?>
+  </main><!-- main -->
+  <?php include 'footer.php' ?>
